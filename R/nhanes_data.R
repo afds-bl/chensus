b04bebda96293dd37508177d309fde851fab5072
souad.guemghar <souad.guemghar@bl.ch>
2023-01-25 08:18:44 +0100

    Added roxygen doc for dataset

---
+++ R/nhanes_data.R
@@ -0,0 +1,18 @@
+#' National Health and Nutrition Examination Survey (NHANES)
+#'
+#' Demogrpahic survey data from NHANES 2015 to 2016, with data on 9307
+#' participants, including sampling weights.
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @references
+#' (\href{https://wwwn.cdc.gov/nchs/nhanes/}{CDC})
+#'
+#' @source \href{https://wwwn.cdc.gov/Nchs/Nhanes/2015-2016/DEMO_I.htm}{NHANES 2015-2916}
+#'
+#' @examples
+#' glimpse(nhanes)
+#' table(nhanes_15_16$edu_level)
+"nhanes"
