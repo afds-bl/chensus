531579e74ea26714710f37a4411e6e0d63d1d548
souad.guemghar <souad.guemghar@bl.ch>
2023-01-25 15:07:33 +0100

    Used stylr to style package

--- R/estimate_pop_cens.R
+++ R/estimate_pop_cens.R
@@ -24,51 +24,66 @@
 #'  }
 #'
 #' @examples
-#' estimate_pop_cens(data = nhanes,
-#' weight_colname = "weights",
-#' strata_variable = "strata",
-#' condition_col = "gender")
+#' estimate_pop_cens(
+#'   data = nhanes,
+#'   weight_colname = "weights",
+#'   strata_variable = "strata",
+#'   condition_col = "gender"
+#' )
 #'
 #' @import dplyr
 #'
 #' @export
 
 estimate_pop_cens <- function(data, weight_colname,
-                                   strata_variable = "zone",
-                                   condition_col = NULL) {
+                              strata_variable = "zone",
+                              condition_col = NULL) {
   # Summarise by strata
-  data <- summarise_pop_cens(data = data, strata_variable = strata_variable,
-                                    weight_colname = weight_colname,
-                                 mh_col = "mh", Nh_col = "Nh") %>%
+  data <- summarise_pop_cens(
+    data = data, strata_variable = strata_variable,
+    weight_colname = weight_colname,
+    mh_col = "mh", Nh_col = "Nh"
+  ) %>%
     # First summation term (1)
-    mutate(T1h = mh/(mh - 1) * (1 - mh / Nh)) %>%
+    mutate(T1h = mh / (mh - 1) * (1 - mh / Nh)) %>%
     # Summarise by strata and conditions
-    summarise_pop_cens(., strata_variable = c(strata_variable, condition_col),
-                              weight_colname = weight_colname,
-                           mh_col = "mhc", Nh_col = "Nhc") %>%
+    summarise_pop_cens(.,
+      strata_variable = c(strata_variable, condition_col),
+      weight_colname = weight_colname,
+      mh_col = "mhc", Nh_col = "Nhc"
+    ) %>%
     # Second summation term 1/2
-    mutate(T1hc = (mh - mhc) * (Nhc / mh) ^2)
+    mutate(T1hc = (mh - mhc) * (Nhc / mh)^2)
 
   data %>%
     group_by(across(c(all_of(strata_variable), all_of(condition_col)))) %>%
     # Second summation term 2/2
-    summarise(T2hc = sum((.data[[weight_colname]] - Nhc/mh)^2)) %>%
-    left_join(distinct(data, across(c(all_of(strata_variable),
-                                      all_of(condition_col), T1h, T1hc, mhc, Nhc)))
-              , ., by = c(strata_variable, condition_col)) %>%
+    summarise(T2hc = sum((.data[[weight_colname]] - Nhc / mh)^2)) %>%
+    left_join(
+      distinct(data, across(c(
+        all_of(strata_variable),
+        all_of(condition_col), T1h, T1hc, mhc, Nhc
+      ))),
+      .,
+      by = c(strata_variable, condition_col)
+    ) %>%
     group_by(across(all_of(condition_col))) %>%
-    # Variance estimate
-    summarise(vhat = sum(T1h * (T1hc + T2hc)),
-              # Population estimate
-              total = sum(Nhc),
-              # True occurrence in survey sample
-              occ = sum(mhc)) %>%
-    # Standard deviation
-    mutate(sd = sqrt(vhat),
-           # Absolute confidence interval
-           ci = sd * qnorm(0.975),
-           # Percent confidence interval
-           ci_per = ci / total * 100) %>%
+    summarise(
+      # Variance estimate
+      vhat = sum(T1h * (T1hc + T2hc)),
+      # Population estimate
+      total = sum(Nhc),
+      # True occurrence in survey sample
+      occ = sum(mhc)
+    ) %>%
+    mutate(
+      # Standard deviation
+      sd = sqrt(vhat),
+      # Absolute confidence interval
+      ci = sd * qnorm(0.975),
+      # Percent confidence interval
+      ci_per = ci / total * 100
+    ) %>%
     # Order as desired
     select(all_of(condition_col), total, vhat, occ, sd, ci, ci_per)
 }
--- R/summarise_pop_cens.R
+++ R/summarise_pop_cens.R
@@ -15,18 +15,20 @@
 #' @returns Tibble \code{data} augmented with columns \code{mh_col} and \code{Nhcol}
 #'
 #' @examples
-#' summarise_pop_cens(nhanes, weight_colname = "weights", strata_variable = "strata") %>% glimpse()
-#'
+#' summarise_pop_cens(nhanes, weight_colname = "weights", strata_variable = "strata") %>%
+#'   glimpse()
 #'
 #' @import dplyr
 #'
 #' @export
 
 summarise_pop_cens <- function(data, weight_colname, strata_variable,
-                               mh_col = "mh", Nh_col = "Nh"){
+                               mh_col = "mh", Nh_col = "Nh") {
   data %>%
     group_by(across(all_of(strata_variable))) %>%
-    summarise({{mh_col}} := n(), # number of participants per stratum
-              {{Nh_col}} := sum(.data[[weight_colname]])) %>% # Total of weights per stratum
+    summarise(
+      {{ mh_col }} := n(), # number of participants per stratum
+      {{ Nh_col }} := sum(.data[[weight_colname]]) # Total of weights per stratum
+    ) %>%
     left_join(data, ., by = strata_variable)
 }
