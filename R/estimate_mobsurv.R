0d512763e805e560dacc8e392d5dcc4e0567b70e
souad.guemghar <souad.guemghar@bl.ch>
2023-02-01 16:44:29 +0100

    Added a comment

--- R/estimate_mobsurv.R
+++ R/estimate_mobsurv.R
@@ -18,6 +18,7 @@
 #' }
 #'
 #' @examples
+#' # We can use the nhanes dataset as an example even if it only contains population data
 #' library(dplyr)
 #' estimate_mobsurv(c("annual_household_income", "annual_family_income"),
 #'   data = nhanes, weight = "weights"
@@ -29,12 +30,12 @@
 #'
 estimate_mobsurv <- function(object, data, weight, cf = 1.14, alpha = 0.05) {
   object %>%
-    purrr::set_names() %>%
+    purrr::set_names() %>% # Output is a list whose elements are named using object
     purrr::map(~ data %>%
       filter(.data[[.x]] >= 0) %>%
       summarise(
         wmean = weighted.mean(x = .data[[.x]], w = .data[[weight]]),
         ci = cf * sqrt(sum(.data[[weight]] * (.data[[.x]] - wmean)^2) / (sum(.data[[weight]]) - 1) / n()) * qnorm(1 - alpha / 2)
       )) %>%
-    purrr::list_rbind(names_to = "id")
+    purrr::list_rbind(names_to = "id") # Convert into a table
 }
