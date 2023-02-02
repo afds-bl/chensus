919319096bc234b46db561ea964cc1e26670cd3e
souad.guemghar <souad.guemghar@bl.ch>
2023-02-02 11:26:02 +0100

    Added number of responses nc

--- R/estimate_mobsurv.R
+++ R/estimate_mobsurv.R
@@ -14,6 +14,7 @@
 #' \itemize{
 #' \item{\code{id}: }{estimated item}
 #' \item{\code{wmean}: }{weighted mean estimate}
+#' \item{\code{nc}: }{number of responses in the survey}
 #' \item{\code{ci}: }{confidence interval estimate}
 #' }
 #'
@@ -46,7 +47,8 @@ estimate_mobsurv <- function(object, data, weight, cf = 1.14, alpha = 0.05) {
       filter(.data[[.x]] >= 0) %>%
       summarise(
         wmean = weighted.mean(x = .data[[.x]], w = .data[[weight]]),
-        ci = cf * sqrt(sum(.data[[weight]] * (.data[[.x]] - wmean)^2) / (sum(.data[[weight]]) - 1) / n()) * qnorm(1 - alpha / 2)
+        nc = n(),
+        ci = cf * sqrt(sum(.data[[weight]] * (.data[[.x]] - wmean)^2) / (sum(.data[[weight]]) - 1) / nc) * qnorm(1 - alpha / 2)
       )) %>%
     purrr::list_rbind(names_to = "id") # Convert into a table
 }
