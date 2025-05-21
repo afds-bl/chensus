a2caa46341ea64408d3c6d235e71303ab9d3bc4f
Guemghar, Souad FKD <souad.guemghar@bl.ch>
2025-05-21 13:41:48 +0000

    Update logo

--- inst/create_logo_image.R
+++ inst/create_logo_image.R
@@ -20,16 +20,20 @@ df <- data.frame(x = x_values, y = y_values)
 
 # Normal distribution with confidence intervals
 mint_1 <- "#317883"
-mint_1 <- "#77b6bf"
+mint_plus2 <- "#1f4b52"
 rot1 <- "#a21628"
+swiss_red <- "#D52B1E"
+dark_grey <- "#444444"
+bl_grau <- "#808080"
+rot0 <- "#cb1c32"
 
 p <- ggplot(df, aes(x = x, y = y)) +
-  geom_line(color = mint_1, linewidth = 1) +                               # Normal distribution curve
+  geom_line(color = mint_plus2, linewidth = 1) +                               # Normal distribution curve
   annotate("segment", x = lower_limit, xend = lower_limit, y = 0, yend = 0.15,  # Shortened lower limit line
-           color = rot1, linewidth = 1.2) +
+           color = rot0, linewidth = 1.2) +
   annotate("segment", x = upper_limit, xend = upper_limit, y = 0, yend = 0.15,  # Shortened upper limit line
-           color = rot1, linewidth = 1.2) +
-  geom_ribbon(aes(ymin = 0, ymax = ifelse(x >= lower_limit & x <= upper_limit, y, NA)), fill = rot1, alpha = 0.2) +
+           color = rot0, linewidth = 1.2) +
+  geom_ribbon(aes(ymin = 0, ymax = ifelse(x >= lower_limit & x <= upper_limit, y, NA)), fill = rot0, alpha = 0.2) +
   ggpubr::theme_transparent() +
   theme(
     axis.title.y = element_blank(),    # Hide y-axis label
@@ -42,53 +46,28 @@ p <- ggplot(df, aes(x = x, y = y)) +
 
 ggsave("man/figures/dnorm.png")
 
-# Create and save sticker
-hexSticker::sticker(
-  subplot = p,
-  package = "chensus",
-  p_size = 20,
-  p_color = "white",
-  p_x = .8,
-  p_y = 1.35,
-  s_x = 0.5,
-  s_y = 1.25,
-  s_width = 3.3,
-  s_height = 3.6,
-  h_fill = "#004774",
-  h_color = "#c0c0c0",
-  h_size = 2.3,
-  white_around_sticker = TRUE,
-  spotlight = FALSE,
-  l_x = 0.75,
-  l_y = 1.25,
-  l_alpha = .8,
-  l_width = 4,
-  # p_family = "inter",
-
-  filename = "man/figures/logo_white.png"
-)
-
 # Load fonts
 # curl issues do not allow fetching font from internet
 # Download then fetch locally
 font_add(family = "Roboto", regular = "~/_home/.coderbar/Roboto-Regular.ttf")
-# font_add_google("Roboto", "roboto") 
+font_add(family = "Montserrat", regular = "~/_home/.coderbar/Montserrat-Regular.ttf")
+font_add(family = "Montserrat Semi", regular = "~/_home//.coderbar/Montserrat-SemiBold.ttf")
 showtext_auto()
 
-# Create the sticker
 sticker(
   p,                               # Plot to display
-  package = "chensus",        # Text label for the package name
-  p_size = 20,                     # Package name font size
-  p_color = "#cb1c32",             # Package name color (matching the red color in the plot)
-  s_x = 1,                         # Position of the plot on the x-axis
-  s_y = 0.8,                       # Position of the plot on the y-axis
-  s_width = 1.4,                   # Width of the plot
-  s_height = 1.2,                  # Height of the plot
-  h_fill = "#e6f2f2",              # Background color of the hex
-  h_color = mint_1,             # Border color of the hex
-  h_size = 2,
-  filename = "man/figures/logo.png"
-  # p_family = "roboto"              # Font family for the package name
+  package = "chensus",             # Text label for the package name
+  p_size = 24,                     # Larger package name font size
+  p_y = 1.3,
+  p_color = rot0,             # Package name color
+  s_x = 1,                        # Plot x position (unchanged)
+  s_y = 0.7,                      # Plot y position (unchanged)
+  s_width = 1.1,                  # Smaller plot width (down from 1.4)
+  s_height = 0.9,                 # Smaller plot height (down from 1.2)
+  h_fill = "#FFFFFF",             # Background color
+  h_color = mint_plus2,           # Border color
+  h_size = 3,                    # Thicker border (up from 2)
+  filename = "man/figures/logo.png",
+  p_family = "Montserrat Semi"            # Optional font family
 )
 
