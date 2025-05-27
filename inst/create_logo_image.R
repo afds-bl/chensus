50f1d49b6e5d0401c2681f432be3ce08504f745b
Guemghar, Souad FKD <souad.guemghar@bl.ch>
2025-05-27 10:09:57 +0000

    Upate logo

--- inst/create_logo_image.R
+++ inst/create_logo_image.R
@@ -26,14 +26,18 @@ swiss_red <- "#D52B1E"
 dark_grey <- "#444444"
 bl_grau <- "#808080"
 rot0 <- "#cb1c32"
+blau_grau <- "#242626"
 
 p <- ggplot(df, aes(x = x, y = y)) +
-  geom_line(color = mint_plus2, linewidth = 1) +                               # Normal distribution curve
+  geom_line(color = blau_grau, linewidth = 0.8) +                               # Normal distribution curve
   annotate("segment", x = lower_limit, xend = lower_limit, y = 0, yend = 0.15,  # Shortened lower limit line
-           color = rot0, linewidth = 1.2) +
+           color = rot0, linewidth = 1) +
   annotate("segment", x = upper_limit, xend = upper_limit, y = 0, yend = 0.15,  # Shortened upper limit line
-           color = rot0, linewidth = 1.2) +
-  geom_ribbon(aes(ymin = 0, ymax = ifelse(x >= lower_limit & x <= upper_limit, y, NA)), fill = rot0, alpha = 0.2) +
+           color = rot0, linewidth = 1) +
+  geom_ribbon(aes(ymin = 0, ymax = ifelse(x >= lower_limit & x <= upper_limit, y, NA)), 
+              fill = rot0, alpha = 0.1) +
+  # Add horizontal line for x-axis
+  geom_hline(yintercept = 0, color = blau_grau, linewidth = 0.4) +
   ggpubr::theme_transparent() +
   theme(
     axis.title.y = element_blank(),    # Hide y-axis label
@@ -49,25 +53,25 @@ ggsave("man/figures/dnorm.png")
 # Load fonts
 # curl issues do not allow fetching font from internet
 # Download then fetch locally
-font_add(family = "Roboto", regular = "~/_home/.coderbar/Roboto-Regular.ttf")
-font_add(family = "Montserrat", regular = "~/_home/.coderbar/Montserrat-Regular.ttf")
-font_add(family = "Montserrat Semi", regular = "~/_home//.coderbar/Montserrat-SemiBold.ttf")
-showtext_auto()
+
+font_add(family = "Arial", regular = "~/.coderbar/arial.ttf", bold = "~/.coderbar/arialbd.ttf")
 
 sticker(
   p,                               # Plot to display
   package = "chensus",             # Text label for the package name
-  p_size = 24,                     # Larger package name font size
+  p_size = 26,                     # Larger package name font size
   p_y = 1.3,
-  p_color = rot0,             # Package name color
+  p_color = rot0,                 # Package name color
   s_x = 1,                        # Plot x position (unchanged)
   s_y = 0.7,                      # Plot y position (unchanged)
   s_width = 1.1,                  # Smaller plot width (down from 1.4)
   s_height = 0.9,                 # Smaller plot height (down from 1.2)
   h_fill = "#FFFFFF",             # Background color
-  h_color = mint_plus2,           # Border color
-  h_size = 3,                    # Thicker border (up from 2)
+  # h_color = mint_1,
+  h_color = rot0,           # Border color
   filename = "man/figures/logo.png",
-  p_family = "Montserrat Semi"            # Optional font family
+  p_family = "Arial",          # Optional font family
+  p_fontface = "bold"
 )
+
 
