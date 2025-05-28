15bb15e45dc28d6d5e6e1c50b34afe81f2c08805
Guemghar, Souad FKD <souad.guemghar@bl.ch>
2025-05-28 12:59:30 +0000

    Add raw NHANES

--- R/nhanes.R
+++ R/nhanes.R
@@ -1,18 +1,32 @@
 #' National Health and Nutrition Examination Survey (NHANES)
 #'
-#' Demographic survey data from NHANES 2015 to 2016, with data on 9307
+#' Demographic survey data from NHANES 2015 to 2016, with data on 9971
 #' participants, including sampling weights.
 #'
-#' @format A data frame with 9307 rows and 13 variables.
-#'
+#' @format A data frame with 9971 rows and 13 variables:
+#' \describe{
+#'   \item{PSU}{SDMVPSU - Masked variance pseudo-PSU}
+#'   \item{weights}{WTINT2YR - Full sample 2 year interview weight}
+#'   \item{strata}{SDMVSTRA - Masked variance pseudo-stratum}
+#'   \item{gender}{RIAGENDR - Gender}
+#'   \item{age}{RIDAGEYR - Age in years at screening }
+#'   \item{birth_country}{DMDBORN4 - Country of birth}
+#'   \item{marital_status}{DMDMARTL - Marital status}
+#'   \item{interview_lang}{SIALANG - Language of interview}
+#'   \item{edu_level}{DMDHREDU - Household reference person's education level}
+#'   \item{household_size}{DMDHHSIZ - Total number of people in the Household}
+#'   \item{family_size}{DMDFMSIZ - Total number of people in the Family}
+#'   \item{annual_household_income}{INDHHIN2 - Annual household income}
+#'   \item{annual_family_income}{INDFMIN2 - Annual family income}
+#' }
 #' @docType data
 #'
 #' @keywords datasets
 #'
 #' @references
-#' (\href{https://wwwn.cdc.gov/nchs/nhanes/}{CDC})
+#' \href{https://wwwn.cdc.gov/nchs/nhanes/}{CDC}
 #'
-#' @source \href{https://wwwn.cdc.gov/Nchs/Nhanes/2015-2016/DEMO_I.htm}{NHANES 2015-2016}
+#' @source \href{https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component=Demographics&CycleBeginYear=2015}{NHANES 2015-2016}
 #'
 #' @examples
 #' library(dplyr)
---
+++ data-raw/nhanes.R
@@ -0,0 +1,43 @@
+# Download the raw Nhanes 2015-2016 data from https://wwwn.cdc.gov/nchs/nhanes/search/DataPage.aspx?Component=Demographics&Cycle=2015-2016
+#  Latest download 28/05/2025
+
+if (!requireNamespace("haven", quietly = TRUE)) {
+  stop("Package 'haven' needed to read the .xpt file. Please install it.")
+}
+
+library(haven)
+library(dplyr)
+library(readr)
+library(forcats)
+
+# Read raw data
+raw_nhanes <- read_xpt(here::here("data-raw", "DEMO_I.xpt"))
+
+# Read coding files
+code_marital_status <- read_tsv(here::here("data-raw", "code_marital_status.tsv"))
+code_edu_level <- read_tsv(here::here("data-raw", "code_edu_level.tsv"))
+
+nhanes <- raw_nhanes %>%
+  select(PSU = SDMVPSU, 
+         weights = WTINT2YR, 
+         strata = SDMVSTRA,
+         gender = RIAGENDR,
+         age = RIDAGEYR,
+         birth_country = DMDBORN4,
+         marital_status = DMDMARTL,
+         interview_lang = SIALANG,
+         edu_level = DMDHREDU,
+         household_size = DMDHHSIZ,
+         family_size = DMDFMSIZ,
+         annual_household_income = INDHHIN2,
+         annual_family_income = INDFMIN2
+         )  %>%
+  mutate(
+    gender = factor(gender, levels = 1:2, labels = c("Male", "Female")),
+    birth_country = factor(birth_country, levels = 1:2, labels = c("US", "Other")),
+    marital_status = factor(marital_status, levels = code_marital_status$Code, labels = code_marital_status$Value),
+    interview_lang = factor(interview_lang, levels = 1:2, labels = c("English", "Spanish")),
+    edu_level = factor(edu_level, levels = code_edu_level$Code, labels = code_edu_level$Value)
+  )
+
+usethis::use_data(nhanes, overwrite = TRUE)
