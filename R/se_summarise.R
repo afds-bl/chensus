048b0ba1f5ee6a027764e45dbf6803660cb6b220
Guemghar, Souad FKD <souad.guemghar@bl.ch>
2025-06-03 08:06:48 +0000

    Suppress dplyr message

--- R/se_summarise.R
+++ R/se_summarise.R
@@ -1,23 +1,20 @@
 #' Summarise Structural Survey Data by Group
-#'
-#' \code{se_summarise()} stratifies and summarises the number of observations and
-#' estimated populations in structural surveys provided by the FSO.
-
-#' \code{se_summarise()} calculates group-wise survey statistics, augmenting the input data with columns for the number of true occurrences and estimated population per group.
+#' 
+#' \code{se_summarise()} calculates group-wise survey statistics, augmenting the input data with columns for the number of observations and estimated population per group.
 #' 
 #' @param data A tibble or a data frame.
 #' @param weight Unquoted name of column containing the survey
 #' weights.
-#' @param ... One or more unquoted grouping variables (e.g., `strata`, `gender`). These define the groups for summarisation.
-#' @param mh_col Character string, desired column name of number of true occurrences per group. Default is "mh".
+#' @param ... One or more unquoted grouping variables (e.g., \code{strata}, \code{gender}). These define the groups for summarisation.
+#' @param mh_col Character string, desired column name of number of observations (respondents) per group. Default is "mh".
 #' @param Nh_col Character string, desired column name of estimated population per group. Default is "Nh".
 #'
 #' @returns
-#' A tibble: the input \code{data} augmented with new columns \code{mh_col} and \code{Nhcol} (or their specified names).
+#' A tibble: the input \code{data} augmented with new columns \code{mh_col} and \code{Nh_col} (or their specified names).
 #' 
 #' @examples
-#' # One stratum variable
-#' library(dplyr)
+#' # One grouping variable
+#' suppressPackageStartupMessages(library(dplyr))
 #' se_summarise(nhanes, weight = weights, strata) |>
 #'   glimpse()
 #' # Two grouping variables
