edca1b49aa9ef6c62526eb3f16284a75256ebfd8
Guemghar, Souad FKD <souad.guemghar@bl.ch>
2025-06-03 08:20:53 +0000

    Test multiple groups

--- tests/testthat/test-se_summarise.R
+++ tests/testthat/test-se_summarise.R
@@ -1,4 +1,4 @@
-test_that("se_summarise computes mh and Nh correctly per stratum", {
+test_that("se_summarise works with one grouping variable", {
   
   # Sample data
   df <- tibble(
@@ -10,18 +10,42 @@ test_that("se_summarise computes mh and Nh correctly per stratum", {
   )
 
   # Expected output
-  expected <- df %>%
-    group_by(zone) %>%
-    mutate(mh = n()) %>%
-    mutate(Nh = sum(weight)) %>%
-    ungroup() %>%
-    distinct(zone, mh, Nh) %>%
+  expected <- df |>
+    group_by(zone) |>
+    mutate(mh = n()) |>
+    mutate(Nh = sum(weight)) |>
+    ungroup() |>
+    distinct(zone, mh, Nh) |>
     arrange(zone)
   
   # Actual result from the function
-  result <- se_summarise(df, weight = "weight", strata = "zone") %>%
-    distinct(zone, mh, Nh) %>%
+  result <- se_summarise(df, weight = weight, zone) |>
+    distinct(zone, mh, Nh) |>
     arrange(zone)
   
+  expect_equal(result, expected)
+})
+
+test_that("se_summarise works with multiple grouping variables", {
+  df <- tibble(
+    zone = rep(c("A", "B"), each = 5),
+    group = rep(c("G1", "G2"), times = 5),
+    weight = c(1.161500, 2.668666, 2.201522, 1.314417, 1.014799, 1.932787, 1.995555, 1.579534, 2.465764, 2.545043)
+  )
+  
+  # Expected output
+  expected <- df |>
+    group_by(zone, group) |>
+    mutate(mh = n()) |>
+    mutate(Nh = sum(weight)) |>
+    ungroup() |>
+    distinct(zone, group, mh, Nh) |>
+    arrange(zone)
+  
+  # Actual result from the function
+  result <- se_summarise(df, weight = weight, zone, group) |> 
+    distinct(zone, group, mh, Nh) |>
+    arrange(zone)
+  expect_true(all(c("mh", "Nh") %in% names(result)))
   expect_equal(result, expected)
 })
