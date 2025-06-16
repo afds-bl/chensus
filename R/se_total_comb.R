3ee9243d70a8058a81f9601276b1e40693d17f78
Guemghar, Souad FKD <souad.guemghar@bl.ch>
2025-06-16 14:44:38 +0000

    Add se_combn() to se_total_comb scritp and unit test

--- R/se_total_comb.R
+++ R/se_total_comb.R
@@ -17,6 +17,7 @@
 #'
 #' @import purrr
 #' @import dplyr
+#' @import forcats
 #'
 #' @export
 #'
@@ -61,4 +62,28 @@ se_total_comb <- function(data, ..., strata, weight) {
         \(v) fct_na_value_to_level(v, "Total")
       )
     )
+}
+
+
+#' Generate All Combinations of Strings from a Character Vector
+#' 
+#' \code{se_combn()} is a helper function used internally to create all possible combinations
+#' of a set of variables. It is typically used for generating grouped summary tables as per Open Government Data formats.
+#' #' @param vars A character vector of variable names.
+#'
+#' @returns
+#' A list of character vectors, each representing a unique combination of the input strings.
+#' The list includes the empty combination of length 0.
+#'
+#' @keywords internal
+#' @importFrom purrr map list_c
+#' @export
+#' 
+#' @examples
+#' vars <- c("gender", "birth_country")
+#' se_combn(vars)
+#' 
+se_combn <- function(vars) {
+  map(0:length(vars), \(n) combn(vars, n, simplify = FALSE)) |>
+    list_c()
 }
