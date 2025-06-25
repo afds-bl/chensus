b3d7d4d4249bc0dda7596167025634f026a82e7d
Guemghar, Souad FKD <souad.guemghar@bl.ch>
2025-06-25 07:48:10 +0000

    Update se_prop for no col

--- tests/testthat/test-se_prop.R
+++ tests/testthat/test-se_prop.R
@@ -48,3 +48,33 @@ test_that("se_prop works with numeric variable by treating it as categorical", {
     regexp = "don't exist"
   )
 })
+
+test_that("se_prop handles no column input correctly", {
+
+    df <- tibble(
+    zone = c("A", "A", "B", "B", "A", "B", "A", "B", "A", "B"),
+    weight = c(1.1, 1.4, 2.0, 0.9, 1.6, 2.2, 1.3, 1.8, 1.0, 2.5)
+  )
+  
+  result <- se_prop(df, weight = weight)
+  
+  expected <- tibble(
+    occ = 10L,
+    prop = 1,
+    vhat = 0,
+    stand_dev = 0,
+    ci = 0,
+    ci_l = 1,
+    ci_u = 1
+  )
+  
+  # Verify results
+  expect_equal(nrow(result), 1)
+  expect_equal(result$occ, 10L)
+  expect_equal(result$prop, 1)
+  expect_equal(result$vhat, 0)
+  expect_equal(result$stand_dev, 0)
+  expect_equal(result$ci, 0)
+  expect_equal(result$ci_l, 1)
+  expect_equal(result$ci_u, 1)
+})
